fc7cd5131b9cd0c777b8d3af711d7377992467a6
李辰龙 <1844424344@qq.com>
2017-04-28 16:37:31 +0800

    Add UnitTest

---
+++ MessageHubTests/signalr/MessageHubTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using signalr.MessageHub;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace signalr.MessageHub.Tests
+{
+    [TestClass()]
+    public class MessageHubTests
+    {
+        [TestMethod()]
+        public void MessageHubTest()
+        {
+            Assert.IsNotNull(MessageHub.laneList);
+            Assert.IsNotNull(MessageHub.serverList);
+            Assert.IsTrue(MessageHub.QueueList.Count==0);
+        }
+        [TestMethod()]
+        public void ClearTest()
+        {
+            Assert.IsTrue(MessageHub.QueueList.Count == 0);
+
+        }
+        [TestMethod()]
+        public void F5Test()
+        {
+            Assert.IsNotNull(MessageHub.laneList);
+            Assert.IsNotNull(MessageHub.serverList);
+        }
+    }
+}
